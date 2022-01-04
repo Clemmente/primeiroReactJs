ea24eb5877b77680b2d5d04e70fe5a60e1b69615
Clemmente <rafael_7072@hotmail.com>
2022-01-04 16:44:28 -0300

    primeiro envio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+return(
+  <>
+  <h1>Primeira Aplicação com ReactJs</h1>
+    <ul>
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+  </ul>
+  </>
+)
+
+}
+export default App;
